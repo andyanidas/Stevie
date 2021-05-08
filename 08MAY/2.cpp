0d82ab05980142ca68f35f4946c27be327d1e245
Lkhagva-erdene <anidas.com@gmail.com>
2021-05-08 09:00:36 +0800

    Add files via upload

---
+++ 08MAY/2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//declare 
+	int x=15,y=10; //int -> integer
+	// = assignment operator
+	
+	x = y + 10;
+	
+	cout<<"x="<<x;
+	
+	return 0;
+}
